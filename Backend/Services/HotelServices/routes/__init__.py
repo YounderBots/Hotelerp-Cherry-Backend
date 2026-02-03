bb40f269a05816c5d9f9efebf3682f96c5d55740
Anandmadhappan <anand.m@younderbots.in>
2026-02-03 17:12:29 +0530

    Updated Incident API Perfectly

--- Backend/Services/HotelServices/routes/__init__.py
+++ Backend/Services/HotelServices/routes/__init__.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
  
 from resources.frontOffice.housekeepingController import router as housekeepingRouter
-from resources.frontOffice.guestController import router as guestRouter
-from resources.frontOffice.RoomIncidentLogController import router as roomincidentlog
+from resources.frontOffice.guestController import router as guestRouter 
 
 router = APIRouter()
  
 router.include_router(housekeepingRouter, prefix='', tags=['Housekeeping']) 
-router.include_router(guestRouter, prefix='', tags=['Guest'])
-router.include_router(roomincidentlog, prefix='', tags=['Roomincidentlog'])
+router.include_router(guestRouter, prefix='', tags=['Guest']) 
