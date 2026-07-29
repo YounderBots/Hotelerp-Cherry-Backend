b8d449a31e4f8e1f9b87e8a49f1520e33ecdc3f3
Anandmadhappan <anand.m@younderbots.in>
2026-07-29 18:39:48 +0530

    Error Updates

--- Backend/Services/HotelServices/configs/dev_config.py
+++ Backend/Services/HotelServices/configs/dev_config.py
@@ -1,6 +1,11 @@
+import os
+
 from configs.base_config import BaseConfig
 class Configuration(BaseConfig):
     DEBUG = True
-    DB_URI = 'mysql+pymysql://root@localhost/hotelerp_hotel'
+    DB_URI = os.getenv(
+        "DB_URI",
+        "mysql+pymysql://root@localhost/hotelerp_hotel",
+    )
   
    
--- Backend/Services/RestaurantServices/configs/dev_config.py
+++ Backend/Services/RestaurantServices/configs/dev_config.py
@@ -1,6 +1,11 @@
+import os
+
 from configs.base_config import BaseConfig
 class Configuration(BaseConfig):
     DEBUG = True
-    DB_URI = 'mysql+pymysql://root@localhost/hotelerp_restaurant'
+    DB_URI = os.getenv(
+        "DB_URI",
+        "mysql+pymysql://root@localhost/hotelerp_restaurant",
+    )
   
    
